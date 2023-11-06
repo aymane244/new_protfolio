4c8fc338c034ce894b8f7d1c1d14283ff3d3692d
aymane244 <a.chnaif2010@gmail.com>
2023-11-06 17:16:02 +0100

    fixing image in hire section

--- src/components/pages/HireMe.js
+++ src/components/pages/HireMe.js
@@ -37,7 +37,7 @@ export default function HireMe(){
                 <div className="col-md-12 px-5 py-3 text-center">
                     <div className="d-flex justify-content-center align-items-center">
                         <div>
-                            <img src="/images/aimane.png" alt="aimane-profile" className="img-fluid img-thumbnail rounded-circle img-size" />
+                            <img src="/images/aimane.png" alt="aimane-profile" className="" />
                         </div>
                         <div className="ms-3">
                             <h4 className="text-color">{lang.myName || uploadLang.myName}</h4>
